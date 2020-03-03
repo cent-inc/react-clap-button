f67f77d66c64bd7a19487401426f98fc44d63fa5
Cameron Hejazi <chejazi@umich.edu>
2020-03-03 11:58:50 -0800

    update total count

--- src/index.js
+++ src/index.js
@@ -36,6 +36,24 @@ const Clap = class extends React.Component {
     const mojs = require('mo-js')
 
     const tlDuration = 300
+    const triangleBurst = new mojs.Burst({
+      parent: this.clapButtonRef.current,
+      radius: { 50: 95 },
+      count: 5,
+      angle: 30,
+      children: {
+        shape: 'polygon',
+        radius: { 6: 0 },
+        scale: 1,
+        stroke: 'rgba(211,84,0 ,0.5)',
+        strokeWidth: 2,
+        angle: 210,
+        delay: 30,
+        speed: 0.2,
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+        duration: tlDuration
+      }
+    })
 
     const circleBurst = new mojs.Burst({
       parent: this.clapButtonRef.current,
@@ -91,7 +109,8 @@ const Clap = class extends React.Component {
       countAnimation,
       // countTotalAnimation,
       scaleButton,
-      circleBurst
+      circleBurst,
+      triangleBurst
     ])
   }
 
@@ -146,7 +165,7 @@ const Clap = class extends React.Component {
           >
             <ClapIcon ref={this.clapIconRef} className='clap--icon' isClicked={isClicked} />
             <ClapCount ref={this.clapCountRef} className='clap--count'>
-              +{count}
+              {count}
             </ClapCount>
             <ClapCountTotal ref={this.clapCountTotalRef} className='clap--count-total'>
               {Number(countTotal).toLocaleString()}
