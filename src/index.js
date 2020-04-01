5b9ec82ff97fb676b470bc445d7fb55235efb71c
Cameron Hejazi <chejazi@umich.edu>
2020-03-31 21:30:07 -0700

    attempt animation fix

--- src/index.js
+++ src/index.js
@@ -47,6 +47,21 @@ const Clap = class extends React.Component {
 
     const tlDuration = 300
 
+    // const countAnimation = new mojs.Html({
+    //   el: this.clapCountRef.current,
+    //   isShowStart: false,
+    //   isShowEnd: true,
+    //   y: { 0: -30 },
+    //   opacity: { 0: 1 },
+    //   duration: tlDuration
+    // }).then({
+    //   opacity: { 1: 0 },
+    //   y: -80,
+    //   delay: tlDuration / 2
+    // })
+
+    const opacityStart = this.props.count > 0 && this.state.unclicked ? 1 : 0
+
     const scaleButton = new mojs.Html({
       el: this.clapButtonRef.current,
       duration: tlDuration,
@@ -58,7 +73,7 @@ const Clap = class extends React.Component {
     clap.style.transform = 'scale(1, 1)'
     this.animationTimeline = new mojs.Timeline()
     this.animationTimeline.add([
-      countAnimation,
+      // countAnimation,
       scaleButton
     ])
   }
